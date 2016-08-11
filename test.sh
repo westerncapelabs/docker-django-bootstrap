e29e31399c60303f4672c846f4a72129d1de40a8
Jamie Hewland <jhewland@gmail.com>
2016-08-11 15:40:31 +0200

    Rather just pass the variant to the test script

--- test.sh
+++ test.sh
@@ -2,17 +2,15 @@
 set -e
 
 function usage() {
-  echo "Usage: $0 IMAGE_TAG"
-  echo "  IMAGE_TAG is the Docker image tag for the django-bootstrap image to test"
+  echo "Usage: $0 VARIANT"
+  echo "  VARIANT is the type of django-bootstrap image to test"
 }
 
-IMAGE_TAG="$1"
+VARIANT="$1"
 shift || { usage >&2; exit 1; }
 
-TAG="${IMAGE_TAG##*:}"
-
 # Build example project image, run and wait for it to start
-docker build --tag mysite --file "example/$TAG.dockerfile" example
+docker build --tag mysite --file "example/$VARIANT.dockerfile" example
 docker run --detach --name mysite -p 8000:8000 mysite
 sleep 5
 
