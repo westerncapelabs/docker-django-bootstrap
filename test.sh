7598ae0b954bf3ccfc63de8d694c1845fe8e6a48
Jamie Hewland <jhewland@gmail.com>
2016-08-12 16:34:04 +0200

    Add functions to run/remove containers

--- test.sh
+++ test.sh
@@ -12,11 +12,30 @@ trap '{ set +x; echo; echo FAILED; echo; } >&2' ERR
 
 set -x
 
+CONTAINERS=()
+function docker_run {
+  # Run a detached container temporarily for tests. Removes the container when
+  # the script exits and sleeps a bit to wait for it to start.
+  local container
+  container="$(docker run -d "$@")"
+  CONTAINERS+=("$container")
+  sleep 5
+}
+
+function remove_containers {
+  echo "Stopping and removing containers..."
+  for container in "${CONTAINERS[@]}"; do
+    docker stop "$container"
+    docker rm -f "$container"
+  done
+}
+
 # Build example project image, run and wait for it to start
 docker build --tag mysite --file "example/$VARIANT.dockerfile" example
-docker run --detach --name mysite -p 8000:8000 mysite
-trap "docker stop mysite; docker rm -f mysite; docker rmi -f mysite" EXIT
-sleep 5
+docker_run --name mysite -p 8000:8000 mysite
+
+# Delete remove containers and delete the image
+trap "remove_containers; docker rmi -f mysite" EXIT
 
 # Simple check to see if the site is up
 curl -fsL http://localhost:8000/admin | fgrep '<title>Log in | Django site admin</title>'
